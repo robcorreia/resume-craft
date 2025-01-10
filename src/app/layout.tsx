f0826673180f39cd192c3473279b0f654af942e5
Robson <robccorreia@gmail.com>
2025-01-10 15:18:36 -0300

    setup project

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const fontSans = Nunito_Sans({ subsets: ["latin"], variable: "--font-sans" });
+const fontTitle = Nunito({ subsets: ["latin"], variable: "--font-title" });
+
+export const metadata: Metadata = {
+  title: "Resume Craft",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontTitle.variable,
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="font-title">Welcome to ResumeCraft</h1>
+    </main>
+  );
+}
