bb15a2d8f3c5faf472e5beb49a91fc1b1401f860
Robson <robccorreia@gmail.com>
2025-01-11 16:29:19 -0300

    feat: add user dropdown

---
+++ src/components/pages/dashboard/user-dropdown.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import { LogOut, SquareUser } from "lucide-react";
+
+export const UserDropdown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="w-full gap-2 justify-start px-2">
+          <Avatar className="w-7 h-7 block">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>RC</AvatarFallback>
+          </Avatar>
+          <p>Robson Correia</p>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="center"
+        className="w-[var(--radix-dropdown-menu-trigger-width)]"
+      >
+        <Link passHref href="/dashboard/account">
+          <DropdownMenuItem className="gap-2">
+            <SquareUser size={16} />
+            Configuraçoes de Conta
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem className="gap-2 text-red-500">
+          <LogOut size={16} />
+          Sair
+        </DropdownMenuItem>{" "}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
