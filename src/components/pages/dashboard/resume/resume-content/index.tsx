a56b368acfc18d8371a7700cb2d564bfd23ad14b
Robson <robccorreia@gmail.com>
2025-01-12 12:12:46 -0300

    feat: add resume layout pages

---
+++ src/components/pages/dashboard/resume/resume-content/index.tsx
@@ -0,0 +1,5 @@
+export const ResumeContent = () => {
+  return (
+    <section className="overflow-gidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background "></section>
+  );
+};
